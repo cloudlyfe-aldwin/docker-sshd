1e63a0bf051ec82468eceb26ab387480dbb9c2e0
cloudlyfe-aldwin <cloudlyfe-aldwin@users.noreply.github.com>
2018-10-28 00:09:07 -0400

    Allow for authorized keys

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,11 @@ FROM arm32v6/alpine
 
 LABEL maintainer="https://github.com/hermsi1337"
 
-ENV SSHD_CONFIG_DIR=/etc/ssh
-
-RUN set -xe \
-  && apk --no-cache --update add openssh \
-  && umask 277 \
-  && mkdir -p /root/.ssh \
-  && chmod -R 700 /root/.ssh \
-  && ln -s /root/.ssh /data
+RUN set -xe && \
+    apk --no-cache --update add openssh && \
+    umask 277 && \
+    mkdir -p /root/.ssh && \
+    chmod -R 700 /root/.ssh
 
 COPY entrypoint.sh /sbin/entrypoint.sh
 RUN chmod 755 /sbin/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,18 @@
 # generate host keys if not present
 ssh-keygen -A
 
-# check wether authorized_keys file exists
-if [ ! -f /root/.ssh/authorized_keys  ]; then
-    touch /root/.ssh/authorized_keys
-fi
-
 # check wether a random root-password is provided
 if [ ! -z "${ROOT_PASSWORD}" ] && [ "${ROOT_PASSWORD}" != "root" ]; then
     echo "root:${ROOT_PASSWORD}" | chpasswd
+    sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config
+else
+    echo "root:$(date | md5sum)" | chpasswd
+fi
+
+# copy authorized_keys
+if [ -f /data/authorized_keys ]; then
+    mkdir -p /root/.ssh
+    cp /data/authorized_keys /root/.ssh
 fi
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
